68370fc90a5530a89c3b45d3a8247d0615ce8552
jihwan2843 <jihwan2843@gmail.com>
2023-11-29 13:55:14 +0900

    day page

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -5,7 +5,7 @@ console.log(englishData);
 
 const Main = () => {
   return (
-    <div className="min-h-screen max-w-screen-md mx-auto px-8 pt-20">
+    <div className="container">
       <h1 className="bg-blue-50 shadow-xl text-center text-2xl font-semibold border py-3 w-52">
         Study English
       </h1>
